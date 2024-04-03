9c81c8b9ff507a34e9938192495868363b6ffbb8
Ilya Pribytkov <118817047+SashaNebo@users.noreply.github.com>
2024-04-03 14:24:25 +0300

    fix: path sprite

--- src/components/UI/IconSvg/IconSvg.tsx
+++ src/components/UI/IconSvg/IconSvg.tsx
@@ -1,13 +1,14 @@
 import { FC } from 'react'
 
 import cn from './IconSvg.module.scss'
-import sprite from '../../../assets/images/sprite.svg'
 import { SPRITE_ICONS } from '../../../types'
 
 interface P {
   iconName: SPRITE_ICONS
   supClassName?: string
 }
+
+const sprite = '/time-calculator/src/assets/images/sprite.svg'
 
 const IconSvg: FC<P> = ({ iconName, supClassName }) => (
   <svg className={[supClassName, cn['icon']].join(' ')}>
